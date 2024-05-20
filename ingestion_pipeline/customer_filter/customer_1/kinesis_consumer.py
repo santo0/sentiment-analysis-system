6c14ff76d735f9ed7076d2a3dfebc177b8d65839
santo0 <lemarti3472@gmail.com>
2024-05-20 14:31:50 +0200

    kinesis consumer automated

--- ingestion_pipeline/customer_filter/customer_1/kinesis_consumer.py
+++ ingestion_pipeline/customer_filter/customer_1/kinesis_consumer.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from boto3.dynamodb.conditions import Key
 import boto3
 import json
 import logging
@@ -7,6 +8,7 @@ import base64
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+# Initialize AWS clients
 kinesis_client = boto3.client('kinesis', )
 dynamodb_client = boto3.client('dynamodb',)
 dynamodb_resource = boto3.resource('dynamodb',)
@@ -30,21 +32,19 @@ def read_kinesis_records(event):
 
 
 def process_record(record):
-    b64_data = record['kinesis']['data']
-    decoded_data = base64.b64decode(b64_data).decode('utf-8')
-    print(decoded_data)
+    data = record['data'].decode('latin-1')  # Assuming data is encoded in UTF-8
+    print(type(data), data)
     # convert data to json
-    tweet_id = str(json.loads(decoded_data)['ids'])
+    tweet_id = str(json.loads(data)['ids'])
     item = {
         'tweet_id': {'N': tweet_id},
         'creation_date': {'S': datetime.utcnow().isoformat()},
-        'tweet': {'S': decoded_data},
+        'tweet': {'S': data},
     }
     print(item)
 
     # Put item into DynamoDB
     dynamodb_client.put_item(TableName=table_name, Item=item)
-
 
 def lambda_handler(event, context):
     read_kinesis_records(event)
--- terraform/modules/data_ingestion/pipeline.tf
+++ terraform/modules/data_ingestion/pipeline.tf
@@ -23,20 +23,36 @@ data "aws_iam_role" "lab_role" {
   name = "LabRole"
 }
 
-
-data "archive_directory" "ingestion_pipeline_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/data_ingestion"
-  output_path = "${path.module}/data_ingestion.zip"
+data "archive_file" "mock_api_zip" {
+  type = "zip"
+  source_file = "../ingestion_pipeline/mock_twitter_api/stateless_tweet_retriever.py"
+  output_path = "./files/mock_api.zip"
 }
 
+data "archive_file" "kinesis_producer_zip" {
+  type = "zip"
+  source_file = "../ingestion_pipeline/kinesis_producer/kinesis_producer.py"
+  output_path = "./files/producer.zip"
+}
+# TODO: Update this to correct customer
+data "archive_file" "customer_1_filter_zip" {
+  type = "zip"
+  source_file = "../ingestion_pipeline/customer_filter/kinesis_consumer.py"
+  output_path = "./files/consumer_customer_1.zip"
+}
+# TODO: UPDATE THIS TO CORRECT CUSTOMER
+data "archive_file" "batch_loader_customer_1_zip" {
+  type = "zip"
+  source_file = "../ingestion_pipeline/batch_loader/table_consumer.py"
+  output_path = "./files/batch_customer_1.zip"
+}
 
 resource "aws_lambda_function" "tweeter_api_lambda" {
   function_name = "tweeter_api_lambda"
   runtime       = "python3.8"
   handler       = "stateless_tweet_retriever.lambda_handler"
-  filename      = data.archive_directory.ingestion_pipeline_zip.output_path
-  source_code_hash = data.archive_directory.ingestion_pipeline_zip.output_base64sha256
+  filename      = data.archive_file.mock_api_zip.output_path
+  source_code_hash = data.archive_file.mock_api_zip.output_base64sha256
   role          = data.aws_iam_role.lab_role.arn
   timeout       = 300
   memory_size   = 512
@@ -46,8 +62,8 @@ resource "aws_lambda_function" "kinesis_producer" {
   function_name = "kinesis_producer"
   runtime       = "python3.8"
   handler       = "kinesis_producer.lambda_handler"
-  filename      = data.archive_directory.ingestion_pipeline_zip.output_path
-  source_code_hash = data.archive_directory.ingestion_pipeline_zip.output_base64sha256
+  filename      = data.archive_file.kinesis_producer_zip.output_path
+  source_code_hash = data.archive_file.kinesis_producer_zip.output_base64sha256
   role          = data.aws_iam_role.lab_role.arn
   timeout       = 300
   memory_size   = 512
@@ -57,8 +73,8 @@ resource "aws_lambda_function" "kinesis_consumer" {
   function_name = "kinesis_consumer"
   runtime       = "python3.8"
   handler       = "kinesis_consumer.lambda_handler"
-  filename      = data.archive_directory.ingestion_pipeline_zip.output_path
-  source_code_hash = data.archive_directory.ingestion_pipeline_zip.output_base64sha256
+  filename      = data.archive_file.customer_1_filter_zip.output_path
+  source_code_hash = data.archive_file.customer_1_filter_zip.output_base64sha256
   role          = data.aws_iam_role.lab_role.arn
   timeout       = 300
   memory_size   = 512
@@ -93,9 +109,25 @@ resource "aws_lambda_function" "table_consumer_1" {
   function_name = "table_consumer_1"
   runtime       = "python3.8"
   handler       = "table_consumer.lambda_handler"
-  filename      = data.archive_directory.ingestion_pipeline_zip.output_path
-  source_code_hash = data.archive_directory.ingestion_pipeline_zip.output_base64sha256
+  filename      = data.archive_file.batch_loader_customer_1_zip.output_path
+  source_code_hash = data.archive_file.batch_loader_customer_1_zip.output_base64sha256
   role          = data.aws_iam_role.lab_role.arn
   timeout       = 300
   memory_size   = 512
+}
+
+
+resource "aws_scheduler_schedule" "batch_1_cron" {
+  name = "batch_1_scheduler"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression = "cron(*/1 * * * ? *)" # run every 1 minute
+
+  target {
+    arn      = aws_lambda_function.table_consumer_1.arn
+    role_arn = data.aws_iam_role.lab_role.arn
+  }
 }
