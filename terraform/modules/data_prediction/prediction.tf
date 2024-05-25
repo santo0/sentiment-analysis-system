36718064825b353c7a870bad25e75e132873dfde
Ubuntu <ubuntu@ip-172-31-43-105.ec2.internal>
2024-05-25 12:09:59 +0000

    prediction working

--- terraform/modules/data_prediction/prediction.tf
+++ terraform/modules/data_prediction/prediction.tf
@@ -2,17 +2,56 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_iam_role" "iam_role" {
+data "aws_iam_instance_profile" "instance_profile" {
   name = "LabInstanceProfile"
+}
+
+# Define the security group
+resource "aws_security_group" "prediction_sg" {
+  name        = "ccbda-prediction-sg"
+  description = "Security group for prediction instance"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTP traffic"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow SSH traffic"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ccbda-prediction-sg"
+  }
 }
 
 resource "aws_instance" "prediction" {
   ami           = "ami-04b70fa74e45c3917"  # Update this to an Ubuntu AMI ID for your region
   instance_type = "m5.large"
-  arn = data.iam_role.arn
+  iam_instance_profile = data.aws_iam_instance_profile.instance_profile.name
+  key_name = "vockey"
+  security_groups = [aws_security_group.prediction_sg.name]
 
   user_data = <<-EOF
               #!/bin/bash
+              sudo apt install unzip
+              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+              unzip awscliv2.zip
+              sudo ./aws/install
               sudo apt-get update -y
               sudo apt-get install -y docker.io
               sudo systemctl start docker
